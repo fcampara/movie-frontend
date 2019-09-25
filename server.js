073426c24d162ad3352818a93262c7c23a153193
Felipe Campara <fcamparasilva@gmail.com>
2019-09-24 22:12:30 -0400

    Feat (Deploy): Create deploy files to heroku

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const
   express = require('express'),
-  path = require('path'),
   serveStatic = require('serve-static'),
   history = require('connect-history-api-fallback'),
   port = process.env.PORT || 5000
@@ -8,5 +7,5 @@ const
 const app = express()
 
 app.use(history())
-app.use(serveStatic(__dirname + '/dist/pwa'))
+app.use(serveStatic(__dirname + '/dist/spa'))
 app.listen(port)
